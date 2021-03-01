19bdc458f1d0a6f0d0a85c2f3233e167a19d00bd
Sergio Casizzone <jambtc@gmail.com>
2021-03-01 12:46:25 +0100

    added phpseclib 2.0

---
+++ protected/extensions/phpseclib/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a4779aaa8b9ad6c611f0781760699695f4da46f4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a4779aaa8b9ad6c611f0781760699695f4da46f4',
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '2.0.30',
+      'version' => '2.0.30.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '136b9ca7eebef78be14abf90d65c5e57b6bc5d36',
+    ),
+  ),
+);
